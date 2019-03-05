48a49d6796ad5faa152aab2d3863bb95df01646d
Manuel G. Araujo A <geronimoarteaga@gmail.com>
2019-03-05 02:33:07 -0400

    Update Index for Marzo 2019 ...

--- sync/js/calendar.js
+++ sync/js/calendar.js
@@ -62,7 +62,7 @@ function createCalendarDay(day, month) {
   var a = document.createElement("a");
 
   a.innerHTML = day;
-  a.href = today.getFullYear() + "/" + monthAsLink(month) + "/" + dayAsLink(day);
+  a.href = today.getFullYear() + "/" + monthAsLink(month) + "/" + dayAsLink(day) + "/index.htm";
 
   var currentDate = new Date();
   currentDate.setDate(day);
